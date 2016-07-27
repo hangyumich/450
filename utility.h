f1e35aeee85b2108ee1519a04b9de3a838c7746e
Hang <hangy@umich.edu>
2016-07-27 14:43:58 +0800

    change cut image

--- utility.h
+++ utility.h
@@ -19,6 +19,6 @@ int vectorsum(std::vector<int> v);
 void ReadImagesInfo(std::string file_path, std::unordered_map<std::string, std::vector<cv::Rect>>&);
 void draw_rects(cv::Mat *frame, std::vector<cv::Rect> faces, int color, int thickness, int lineType);
 bool overlap_bool(cv::Rect rectA, std::vector<cv::Rect> answer, int threshold, bool percentage_over_itself);
-std::vector<std::string> getImageNames (void);
+std::vector<std::string> getImageNames (std::string filename);
 
 #endif
